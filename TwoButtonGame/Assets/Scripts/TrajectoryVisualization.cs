416987021fca98a89f8ec694ac54691a893a1e79
Your Name <you@example.com>
2017-11-11 17:58:22 -0800

    Added accelerating trajectory visualization
    Signed-off-by: Scott Sewell <scott.sewell@mail.mcgill.ca>

--- TwoButtonGame/Assets/Scripts/TrajectoryVisualization.cs
+++ TwoButtonGame/Assets/Scripts/TrajectoryVisualization.cs
@@ -4,32 +4,76 @@ using Framework.Interpolation;
 
 public class TrajectoryVisualization
 {
-    private GameObject trajectoryBox;
     private GameObject crosshairBox;
+    private GameObject accelerationCrosshairBox;
+    private GameObject trajectoryBox;
+    private GameObject[] accelerationBoxes;
+    private Vector3 lastVelocity;
+
+    private static readonly int segments = 10;
+    private static readonly int frames = 4;
 
     public TrajectoryVisualization(bool interpolated)
     {
-        trajectoryBox = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        Object.Destroy(trajectoryBox.GetComponent<Collider>());
-        trajectoryBox.AddComponent<TransformInterpolator>();
-        trajectoryBox.GetComponent<MeshRenderer>().material.color = Color.blue;
-
-        crosshairBox = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        Object.Destroy(crosshairBox.GetComponent<Collider>());
-        crosshairBox.AddComponent<TransformInterpolator>();
+        crosshairBox = makeBox(Color.green);
         crosshairBox.transform.localScale = new Vector3(1, 1, 1);
-        crosshairBox.GetComponent<MeshRenderer>().material.color = Color.green;
+
+        accelerationCrosshairBox = makeBox(Color.yellow);
+        accelerationCrosshairBox.transform.localScale = new Vector3(1, 1, 1);
+        
+        trajectoryBox = makeBox(Color.blue);
+
+        accelerationBoxes = new GameObject[segments];
+        for (int i = 0; i < segments; i++)
+        {
+            accelerationBoxes[i] = makeBox(Color.Lerp(Color.red, Color.magenta, i / (float) segments));
+        }
+
+        lastVelocity = Vector3.zero;
+    }
+
+    public GameObject makeBox(Color color)
+    {
+        GameObject box = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        Object.Destroy(box.GetComponent<Collider>());
+        box.AddComponent<TransformInterpolator>();
+        box.GetComponent<MeshRenderer>().material.color = color;
+        return box;
     }
 
     public void FixedUpdateTrajectory(Vector3 position, Vector3 velocity)
     {
+        Vector3 acceleration = velocity - lastVelocity;
         Vector3 endpoint = position + velocity;
 
-        trajectoryBox.transform.localScale = new Vector3(0.5f, 0.5f, velocity.magnitude);
-        trajectoryBox.transform.position = (position + endpoint) / 2;
-        trajectoryBox.transform.LookAt(endpoint);
-
+        positionBox(trajectoryBox, position, endpoint);
         crosshairBox.transform.position = endpoint;
+
+        Vector3 estimatedPosition = position;
+        Vector3 newEstimatedPosition = position;
+        Vector3 estimatedVelocity = velocity;
+        for (int i = 0; i < segments; i++)
+        {
+            for (int j = 0; j < frames; j++)
+            {
+                //estimatedVelocity += Time.fixedDeltaTime * acceleration;
+                estimatedVelocity += acceleration;
+                newEstimatedPosition += Time.fixedDeltaTime * estimatedVelocity;
+            }
+
+            positionBox(accelerationBoxes[i], estimatedPosition, newEstimatedPosition);
+            estimatedPosition = newEstimatedPosition;
+        }
+        accelerationCrosshairBox.transform.position = newEstimatedPosition;
+
+        lastVelocity = velocity;
+    }
+
+    public void positionBox(GameObject box, Vector3 start, Vector3 end)
+    {
+        box.transform.localScale = new Vector3(0.5f, 0.5f, (end - start).magnitude);
+        box.transform.position = (start + end) / 2;
+        box.transform.LookAt(end);
     }
 
     public void EndVisualization()
@@ -41,6 +85,13 @@ public class TrajectoryVisualization
         if (crosshairBox)
         {
             Object.Destroy(crosshairBox);
+        }
+        for (int i = 0; i < segments; i++)
+        {
+            if (accelerationBoxes[i])
+            {
+                Object.Destroy(accelerationBoxes[i]);
+            }
         }
     }
     
